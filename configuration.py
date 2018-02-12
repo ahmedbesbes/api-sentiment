0e940f0fcbc798a107f87c92870c2f4993897c76
nkooli <nkooli@pagesjaunes.fr>
2018-02-12 12:10:27 +0100

    code refactoring

--- configuration.py
+++ configuration.py
@@ -4,11 +4,9 @@ filename_reviews="./data/reviews.txt"
 filename_aspects="./data/aspects.txt"
 filename_csv="./data/sentiment_review.csv"
 
-model_mem='./mem_absa/models/test_model'
 
 filename_pj_aspects="./data/asp_pj.txt"
 filename_pj_sentiments="./data/sent_pj.txt"
 
-pathSeqTagging='./sequence_tagging'
 
 pathFasttext='./data/model_pyfasttext100.bin'
